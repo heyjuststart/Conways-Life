0d28b8edb81136bcd9de2215aad7c42d799da191
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-15 19:53:00 -0400

    add grid to bg canvas

--- mvp/src/Game.js
+++ mvp/src/Game.js
@@ -2,6 +2,12 @@ import blue from '@material-ui/core/colors/blue';
 import React, { useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
+const params = {
+  lineWidth: 1.0,
+  cellWidth: 10,
+  cellHeight: 10
+};
+
 const drawForeground = canvas => {
   const context = canvas.getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
@@ -12,7 +18,31 @@ const drawBackground = canvas => {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = blue[50];
   context.fillRect(0, 0, canvas.width, canvas.height);
-}
+
+  // draw the grid
+  // first translate half a pixel for pixel accuracy
+  // https://stackoverflow.com/questions/13879322/drawing-a-1px-thick-line-in-canvas-creates-a-2px-thick-line
+  context.translate(0.5, 0.5);
+
+  // starting at 0, iterate by desired cellWidth
+  context.lineWidth = params.lineWidth;
+  context.beginPath();
+  for (let x = params.cellWidth; x < canvas.width; x += params.cellWidth) {
+    context.moveTo(x, 0);
+    context.lineTo(x, canvas.height);
+  }
+
+  // same for y
+  for (let y = params.cellHeight; y < canvas.height; y += params.cellHeight) {
+    context.moveTo(0, y);
+    context.lineTo(canvas.width, y);
+  }
+
+  context.stroke();
+
+  // restore translation
+  context.setTransform(1, 0, 0, 1, 0, 0);
+};
 
 const resizeCanvas = canvas => {
   canvas.height = canvas.parentElement.clientHeight;
@@ -49,7 +79,7 @@ const Game = () => {
     const b = backgroundRef.current;
     resizeCanvas(c);
     resizeCanvas(b);
-    drawBackground(b)
+    drawBackground(b);
     drawForeground(c);
   };
 
@@ -58,7 +88,7 @@ const Game = () => {
     const b = backgroundRef.current;
     resizeCanvas(c);
     resizeCanvas(b);
-    drawBackground(b)
+    drawBackground(b);
     drawForeground(c);
   }, []);
 
