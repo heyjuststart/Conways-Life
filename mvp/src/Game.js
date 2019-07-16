3e377438e4474c683430a0da718243078794606b
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-16 18:06:42 -0400

    implement first-pass rules and stepping
    First pass at implementing the rules for game of life.
    I'm storing cell data as an array of integers, 0 meaning dead, 1
    meaning alive.
    
    As you click the cycle button, the nextCellState s calculated via
    calculateNextState which takes an array of 0's and 1's and returns a new
    array of the next state.
    
    Also implemented a getIndexForGridCoords function which takes grid
    coordinates in the form (row, column) and returns the corresponding
    index if it exists, or null if not.

--- mvp/src/Game.js
+++ mvp/src/Game.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -26,12 +26,88 @@ const params = {
   cellWidth: 10,
   cellHeight: 10,
   widthInCells: 40,
-  heightInCells: 20
+  heightInCells: 40
 };
 
-const drawForeground = canvas => {
+const initalCells = Array(params.widthInCells * params.heightInCells).fill(0);
+
+const drawForeground = (canvas, cells) => {
   const context = canvas.getContext('2d');
   context.clearRect(0, 0, canvas.width, canvas.height);
+  // context.beginPath();
+  if (cells !== undefined) {
+    for (let i = 0; i < cells.length; i++) {
+      if (cells[i] === 1) {
+        const x = (i % params.widthInCells) * params.cellWidth;
+        const y = Math.floor(i / params.widthInCells) * params.cellHeight;
+        context.fillRect(x, y, params.cellWidth, params.cellHeight);
+      }
+    }
+  }
+};
+
+const getIndexForGridCoords = (row, col) => {
+  if (row < 0 || row > params.heightInCells - 1) {
+    return null;
+  }
+
+  if (col < 0 || col > params.widthInCells - 1) {
+    return null;
+  }
+
+  return row * params.widthInCells + col;
+};
+
+// calculate the next cycle
+const calculateNextState = cells => {
+  const newCells = Array(cells.length).fill(0);
+
+  // iterate over the array of given cells
+  // count their neighbors
+  for (let i = 0; i < cells.length; i++) {
+    let neighborCount = 0;
+
+    const row = Math.floor(i / params.widthInCells);
+    const column = i % params.widthInCells;
+
+    // check top row
+    for (let j = column - 1; j < column + 2; j++) {
+      const index = getIndexForGridCoords(row - 1, j);
+      if (cells[index] === 1) {
+        neighborCount += 1;
+      }
+    }
+
+    // check left and right
+    const leftIndex = getIndexForGridCoords(row, column - 1);
+    neighborCount += leftIndex !== null && cells[leftIndex] === 1 ? 1 : 0;
+
+    const rightIndex = getIndexForGridCoords(row, column + 1);
+    neighborCount += rightIndex !== null && cells[rightIndex] === 1 ? 1 : 0;
+
+    // if(i === 1) {
+    // debugger
+    // }
+
+    // check bottom row
+    // check top row
+    for (let j = column - 1; j < column + 2; j++) {
+      const index = getIndexForGridCoords(row + 1, j);
+      if (cells[index] === 1) {
+        neighborCount += 1;
+      }
+    }
+
+    // Check the rules
+    if (
+      (cells[i] === 1 && (neighborCount === 2 || neighborCount === 3)) ||
+      (cells[i] === 0 && neighborCount === 3)
+    ) {
+      newCells[i] = 1;
+    }
+  }
+
+  return newCells;
 };
 
 const drawBackground = canvas => {
@@ -71,37 +147,81 @@ const resizeCanvas = canvas => {
 };
 
 const Game = () => {
+  // initialize with an empty board
+  const [cells, setCells] = useState(initalCells);
   const classes = useStyles();
   const backgroundRef = useRef(null);
   const foregroundRef = useRef(null);
 
-  const resizeHandler = () => {
+  // handle for window resize event
+  const resizeHandler = useCallback(() => {
     const c = foregroundRef.current;
     const b = backgroundRef.current;
     resizeCanvas(c);
     resizeCanvas(b);
     const wrapper = c.parentNode;
     const paper = wrapper.parentNode;
+
+    // set some heights to account for overlayed canvases
+    // and their borders
     paper.style.height = `${c.height + 34}px`;
     wrapper.style.width = `${c.width}px`;
+    wrapper.style.height = `${c.height + 2}px`;
     drawBackground(b);
     drawForeground(c);
-  };
-
-  useEffect(() => {
-    resizeHandler();
   }, []);
 
-  useEffect(() => {
-    window.addEventListener('resize', resizeHandler);
+  // handle canvas click
+  const canvasClickHandler = useCallback(
+    e => {
+      const { clientY: clickY, clientX: clickX } = e;
+      const canvas = foregroundRef.current;
+      const rect = canvas.getBoundingClientRect();
+      const canvasX = clickX - rect.left;
+      const canvasY = clickY - rect.top;
 
-    return () => window.removeEventListener('resize', resizeHandler);
-  });
+      // how many rows down did we click?
+      const row = Math.floor(canvasY / params.cellHeight);
+      const column = Math.floor(canvasX / params.cellWidth);
+
+      // get the cell for those coordinates
+      const index = params.widthInCells * row + column;
+
+      // copy and mutate before setting new state
+      const nextCells = [...cells];
+      nextCells[index] = nextCells[index] === 0 ? 1 : 0;
+      setCells(nextCells);
+    },
+    [cells]
+  );
+
+  // resize on Mount
+  useEffect(() => {
+    const fgCanvas = foregroundRef.current;
+    resizeHandler();
+    window.addEventListener('resize', resizeHandler);
+    fgCanvas.addEventListener('click', canvasClickHandler);
+
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+      fgCanvas.removeEventListener('click', canvasClickHandler);
+    };
+  }, [resizeHandler, canvasClickHandler]);
+
+  // rerender foreground on cells change
+  useEffect(() => {
+    const canvas = foregroundRef.current;
+    drawForeground(canvas, cells);
+  }, [cells]);
 
   return (
-    <div className={classes.wrapper}>
-      <canvas className={classes.background} ref={backgroundRef} />
-      <canvas className={classes.foreground} ref={foregroundRef} />
+    <div>
+      <div className={classes.wrapper}>
+        <canvas className={classes.background} ref={backgroundRef} />
+        <canvas className={classes.foreground} ref={foregroundRef} />
+      </div>
+      <button onClick={() => setCells(initalCells)}>clear</button>
+      <button onClick={() => setCells(calculateNextState(cells))}>cycle</button>
     </div>
   );
 };
